394ab69c89ad46fedf98d9eaff7d09c0ef365749
Девяткин Федор <devjatkin.fjodr@gmail.com>
2019-12-21 13:03:57 +0400

    Добавьте файлы проекта.

---
+++ UpdateDBConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UpdateDBConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
