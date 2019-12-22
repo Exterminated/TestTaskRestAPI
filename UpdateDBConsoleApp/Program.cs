81702b944faf06e1282c7a7d0c4a728c64bd65c1
Девяткин Федор <devjatkin.fjodr@gmail.com>
2019-12-22 16:52:11 +0400

    Наброски по использованию GeoIP2

--- UpdateDBConsoleApp/Program.cs
+++ UpdateDBConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MaxMind;
+using MaxMind.GeoIP2;
 
 namespace UpdateDBConsoleApp
 {
@@ -6,7 +8,28 @@ namespace UpdateDBConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Try to open GeoIP2 DB");
+            try
+            {
+                using (var reader = new DatabaseReader("H:\\repos\\TestTaskRestAPI\\GeoLite2_BDs\\GeoLite2-Country_20191217"))
+                {
+                    var response = reader.AnonymousIP("85.25.43.84");
+                    Console.WriteLine(response.IsAnonymous);       // true
+                    Console.WriteLine(response.IsAnonymousVpn);    // false
+                    Console.WriteLine(response.IsHostingProvider); // false
+                    Console.WriteLine(response.IsPublicProxy);      // false
+                    Console.WriteLine(response.IsTorExitNode);     // true
+                    Console.WriteLine(response.IPAddress);         // '85.25.43.84'
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in getting data\n{ex.ToString()}");
+            }
+            finally {
+                Console.ReadLine();
+            }
+
         }
     }
 }
